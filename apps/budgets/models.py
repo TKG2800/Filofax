a0ea7176d1270412c5f1e3b894b50c4dcd54be40
TKG2800 <takumi1997iwamatsu@yahoo.co.jp>
2023-11-09 18:27:32 +0900

    add templates

--- apps/budgets/models.py
+++ apps/budgets/models.py
@@ -1,29 +1,52 @@
 from django.db import models
 
 # Create your models here.
-class PaymentCategory(models.Model):
-    name = models.CharField('カテゴリ名', max_length=32)
-
-    def __str__(self):
-        return self.name
-
-
-class Payment(models.Model):
-    date = models.DateField('日付')
-    price = models.IntegerField('金額')
-    category = models.ForeignKey(PaymentCategory, on_delete=models.PROTECT, verbose_name='カテゴリ')
-    description = models.TextField('摘要', null=True, blank=True)
-
+"""
+    TODO:
+         - カテゴリ(Category)
+         - 収入(income)
+            - 固定(fixation)
+            - 変動(change)
+         - 支払い(payment)
+            - 固定(fixation)
+            - 変動(change)
+        - 貯蓄(savings)
+"""
 
 class IncomeCategory(models.Model):
+    """ Class IncomeCategory
+       TODO:
+             - カテゴリ名(name)
+    """
     name = models.CharField('カテゴリ名', max_length=32)
-
     def __str__(self):
         return self.name
 
-
 class Income(models.Model):
+    """ Class Income
+        TODO:
+             - 日付(Date)
+             - 金額(Price)
+             - カテゴリ(Category)
+             - 摘要(description)
+    """
     date = models.DateField('日付')
     price = models.IntegerField('金額')
-    category = models.ForeignKey(IncomeCategory, on_delete=models.PROTECT, verbose_name='カテゴリ')
-    description = models.TextField('摘要', null=True, blank=True)
+    description = models.TextField('摘要')
+    pass
+
+class paymentCategory(models.Model):
+        """ Class Income
+        TODO:
+             - カテゴリ名(name)
+        """
+        name = models.CharField('カテゴリ名', max_length=32)
+         = 
+class payment(models.Model):
+        """ Class Income
+        TODO:
+             - 日付(Date)
+             - 金額(Price)
+             - カテゴリ(Category)
+             - 摘要(description)
+        """
